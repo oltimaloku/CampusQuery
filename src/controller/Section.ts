d5289acbe5d93e9c9ad1bfc7506d152b36cf3dc4
oltimaloku <oltimaloku555@gmail.com>
2024-09-30 21:34:22 -0700

    feat: add constructor for section class

--- src/controller/Section.ts
+++ src/controller/Section.ts
@@ -11,5 +11,27 @@ export default class Section {
 	private readonly audit: number;
 
 	// TODO: add constructor logic
-	// public constructor() {}
+	public constructor(
+		uuid: string,
+		id: string,
+		title: string,
+		instructor: string,
+		dept: string,
+		year: number,
+		avg: number,
+		pass: number,
+		fail: number,
+		audit: number
+	) {
+		this.uuid = uuid;
+		this.id = id;
+		this.title = title;
+		this.instructor = instructor;
+		this.dept = dept;
+		this.year = year;
+		this.avg = avg;
+		this.pass = pass;
+		this.fail = fail;
+		this.audit = audit;
+	}
 }
