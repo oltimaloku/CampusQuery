6f7bf4c6e903d0ad8c0dfe03967c8428fe07dec0
oltimaloku <oltimaloku555@gmail.com>
2024-10-28 23:43:38 -0700

    feat: parse html and find build table

--- src/controller/DatasetProcessor.ts
+++ src/controller/DatasetProcessor.ts
@@ -2,9 +2,12 @@
 import JSZip from "jszip";
 import { InsightError } from "./IInsightFacade";
 import Section from "./Section";
+import Room from "./Room";
+import * as parse5 from "parse5";
+import { ChildNode, Document } from "parse5/dist/tree-adapters/default";
 
 export default class DatasetProcessor {
-	public static async processContent(content: string): Promise<Section[]> {
+	public static async processSectionsContent(content: string): Promise<Section[]> {
 		try {
 			const buf = Buffer.from(content, "base64");
 			const zip = new JSZip();
@@ -50,5 +53,71 @@ export default class DatasetProcessor {
 
 		await Promise.all(promises);
 		return sections;
+	}
+
+	public static async processRoomsContent(content: string): Promise<Room[]> {
+		try {
+			const buf = Buffer.from(content, "base64");
+			const zip = new JSZip();
+			const zipContent = await zip.loadAsync(buf);
+			const rooms = await this.createValidRoomsFromZip(zipContent);
+			if (rooms.length === 0) {
+				throw new InsightError("No valid rooms found");
+			}
+			return rooms;
+		} catch (e) {
+			throw new InsightError("Error processing content: " + e);
+		}
+	}
+
+	private static async createValidRoomsFromZip(zipContent: JSZip): Promise<Room[]> {
+		const rooms: Room[] = [];
+		const promises: Promise<void>[] = [];
+
+		// First get and parse the index.htm file
+		let indexFile: JSZip.JSZipObject | null = null;
+		zipContent.forEach((relativePath: string, file: JSZip.JSZipObject) => {
+			console.log("relativePath: " + relativePath);
+			if (relativePath.endsWith("/index.htm")) {
+				indexFile = file;
+			}
+		});
+
+		if (!indexFile) {
+			return rooms;
+		}
+
+		const indexStringContent = await (indexFile as JSZip.JSZipObject).async("string");
+
+		const indexDoc: Document = parse5.parse(indexStringContent);
+
+		//console.log("indexDoc HTML:", parse5.serialize(indexDoc));
+
+		const buildingTable = this.findBuildingTable(indexDoc.childNodes);
+		if (!buildingTable) {
+			return rooms;
+		}
+		console.log("buildingTable HTML:", buildingTable);
+		return rooms;
+	}
+
+	private static findBuildingTable(nodes: ChildNode[]): ChildNode | null {
+		for (const node of nodes) {
+			// Check if the node has children
+			if ("childNodes" in node) {
+				if (
+					node.nodeName === "table" &&
+					node.attrs?.some((attr) => attr.name === "class" && attr.value === "views-table cols-8")
+				) {
+					return node;
+				}
+				// Search children
+				const result = this.findBuildingTable(node.childNodes);
+				if (result) {
+					return result;
+				}
+			}
+		}
+		return null;
 	}
 }
