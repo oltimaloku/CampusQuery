9110c8fded1d4cd8d573a2a2207908cdbddebae8
oltimaloku <oltimaloku555@gmail.com>
2024-11-20 13:03:00 -0800

    feat: initial markers displayed on map

--- frontend/campus-explorer/src/components/MapContainer.tsx
+++ frontend/campus-explorer/src/components/MapContainer.tsx
@@ -1,8 +1,17 @@
-import React from "react";
-import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
-import { GOOGLE_API_KEY } from "../constants";
+import React, { useState, useEffect } from "react";
+import { GoogleMap, Marker } from "@react-google-maps/api";
+
+interface MarkerData {
+	c2rooms_lat: number;
+	c2rooms_lon: number;
+	c2rooms_fullname: string;
+	c2rooms_shortname: string;
+	c2rooms_address: string;
+}
 
 const MapContainer = () => {
+	const [markers, setMarkers] = useState<MarkerData[]>([]);
+
 	const mapStyles = {
 		height: "600px",
 		width: "100%",
@@ -13,9 +22,59 @@ const MapContainer = () => {
 		lng: -123.246,
 	};
 
+	useEffect(() => {
+		const fetchBuildings = async () => {
+			const query = {
+				WHERE: {},
+				OPTIONS: {
+					COLUMNS: [
+						"c2rooms_shortname",
+						"c2rooms_fullname",
+						"c2rooms_seats",
+						"c2rooms_furniture",
+						"c2rooms_lat",
+						"c2rooms_lon",
+					],
+				},
+			};
+
+			try {
+				const response = await fetch("http://localhost:4321/query", {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify(query),
+				});
+
+				const data = await response.json();
+				console.log(data);
+				if (data.result) {
+					const markerData = data.result as MarkerData[];
+					for (let marker in markerData) {
+						console.log(marker);
+					}
+					setMarkers(markerData);
+				} else {
+					//console.error("Error fetching data:", data.error);
+				}
+			} catch (error) {
+				//console.error("Network error:", error);
+			}
+		};
+
+		fetchBuildings();
+	}, []);
+
 	return (
-		<GoogleMap mapContainerStyle={mapStyles} zoom={10} center={defaultCenter}>
-			<Marker position={defaultCenter} />
+		<GoogleMap mapContainerStyle={mapStyles} zoom={15} center={defaultCenter}>
+			{markers.map((marker, index) => (
+				<Marker
+					key={index}
+					position={{ lat: marker.c2rooms_lat, lng: marker.c2rooms_lon }}
+					title={marker.c2rooms_fullname}
+				/>
+			))}
 		</GoogleMap>
 	);
 };
--- frontend/campus-explorer/src/components/NavBar.tsx
+++ frontend/campus-explorer/src/components/NavBar.tsx
@@ -1,21 +1,21 @@
 import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
-  return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/map">Map</NavLink>
-        </li>
-        <li>
-          <NavLink to="/search">Search</NavLink>
-        </li>
-      </ul>
-    </nav>
-  );
+	return (
+		<nav>
+			<ul>
+				<li>
+					<NavLink to="/">Home</NavLink>
+				</li>
+				<li>
+					<NavLink to="/map">Map</NavLink>
+				</li>
+				<li>
+					<NavLink to="/search">Search</NavLink>
+				</li>
+			</ul>
+		</nav>
+	);
 };
 
 export default NavBar;
